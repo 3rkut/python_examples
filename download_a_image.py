ff9cc36042c6cadda0bf2964cf2534a1c2c633b6
interneturtle <41947333+interneturtle@users.noreply.github.com>
2020-03-08 18:19:43 +0300

    Update download_a_image.py

--- download_a_image.py
+++ download_a_image.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 import urllib.request #import the library
 
-url1 = "https://metrouk2.files.wordpress.com/2017/12/888848052.jpg?w=748&h=505&crop=1&quality=80"  #url1 is the first variable
-url2 = "https://cdn.images.dailystar.co.uk/dynamic/58/photos/561000/620x/Cristiano-Ronaldo-683731.jpg" #2nd variable
-url3 = "https://e00-marca.uecdn.es/assets/multimedia/imagenes/2018/06/07/15283977760213.jpg"  #3rd variable
+url1 = "https://i.ytimg.com/vi/I0MT8SwNa_U/maxresdefault.jpg"  #url1 is the first variable
+url2 = "https://ichef.bbci.co.uk/news/1024/cpsprodpb/184A0/production/_109788499_megan.jpg" #2nd variable
+url3 = "https://thoughtcatalog.files.wordpress.com/2018/05/questionstoaskagirl2.jpg"  #3rd variable
 
 urllist= [url1,url2,url3]  # we created list named urllist and the variables
 number = 1:  #it should be 1 
