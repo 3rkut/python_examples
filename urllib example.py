00810e4328b27d7e41e2b621fea02c2641a28a5e
interneturtle <41947333+interneturtle@users.noreply.github.com>
2020-03-08 17:59:54 +0300

    Update urllib example.py

--- urllib example.py
+++ urllib example.py
@@ -1,11 +1,12 @@
-import urllib.request
+#!/usr/bin/env python3
+import urllib.request #import the library
 
-url1 = "https://metrouk2.files.wordpress.com/2017/12/888848052.jpg?w=748&h=505&crop=1&quality=80"
-url2 = "https://cdn.images.dailystar.co.uk/dynamic/58/photos/561000/620x/Cristiano-Ronaldo-683731.jpg"
-url3 = "https://e00-marca.uecdn.es/assets/multimedia/imagenes/2018/06/07/15283977760213.jpg"
+url1 = "https://metrouk2.files.wordpress.com/2017/12/888848052.jpg?w=748&h=505&crop=1&quality=80"  #url1 is the first variable
+url2 = "https://cdn.images.dailystar.co.uk/dynamic/58/photos/561000/620x/Cristiano-Ronaldo-683731.jpg" #2nd variable
+url3 = "https://e00-marca.uecdn.es/assets/multimedia/imagenes/2018/06/07/15283977760213.jpg"  #3rd variable
 
-urllistesi = [url1,url2,url3]
-say = 1
-for url in urllistesi:
-    urllib.request.urlretrieve(url,"Resim"+str(say)+".jpg")
-    say+=1
+urllist= [url1,url2,url3]  # we created list named urllist and the variables
+number = 1:  #it should be 1 
+for url in urllist:
+    urllib.request.urlretrieve(url,"Picture: "+str(number)+".jpg")
+    number+=1
